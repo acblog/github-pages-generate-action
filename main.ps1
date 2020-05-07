49ab815006ac7d649c59b9d4d170d385e64e9b6e
StardustDL <stardustdl@163.com>
2020-05-07 12:58:32 +0800

    fix replace bug

--- main.ps1
+++ main.ps1
@@ -32,15 +32,23 @@ Write-Output "" > $env:INPUT_DIST/.nojekyll
 if (!$?) {
     exit 1
 }
-$(Get-Content $env:INPUT_DIST/404.html) -Replace "<base href=""/"" />", "<base href=""$env:INPUT_DOMAIN"" />" > $env:INPUT_DIST/404.html
+$(Get-Content $env:INPUT_DIST/404.html) -Replace "<base href=""/"" />", "<base href=""$env:INPUT_DOMAIN"" />" > $env:INPUT_DIST/404_new.html
 if (!$?) {
     exit 1
 }
-$(Get-Content $env:INPUT_DIST/404.html) -Replace "sparedirectEncode(l, 0)", "sparedirectEncode(l, $env:INPUT_SEGMENTCOUNT)" > $env:INPUT_DIST/404.html
+$(Get-Content $env:INPUT_DIST/404_new.html) -Replace "sparedirectEncode(l, 0)", "sparedirectEncode(l, $env:INPUT_SEGMENTCOUNT)" > $env:INPUT_DIST/404.html
 if (!$?) {
     exit 1
 }
-$(Get-Content $env:INPUT_DIST/index.html) -Replace "<base href=""/"" />", "<base href=""$env:INPUT_DOMAIN"" />" > $env:INPUT_DIST/index.html
+Remove-Item $env:INPUT_DIST/404_new.html
+if (!$?) {
+    exit 1
+}
+$(Get-Content $env:INPUT_DIST/index.html) -Replace "<base href=""/"" />", "<base href=""$env:INPUT_DOMAIN"" />" > $env:INPUT_DIST/index_new.html
+if (!$?) {
+    exit 1
+}
+Move-Item $env:INPUT_DIST/index_new.html $env:INPUT_DIST/index.html
 if (!$?) {
     exit 1
 }
